182af0416c98a3f31d758f6842a274c23435a2d5
DanielWoytitzki <danielwoytitzki@icloud.com>
2024-10-25 12:48:53 +0200

    added new images to preload, adjust endboss

--- models/boss-health-bar.class.js
+++ models/boss-health-bar.class.js
@@ -1,20 +1,25 @@
 class BossHealthBar {
     constructor(boss) {
         this.boss = boss;
-        this.width = 100;  // Breite des Lebensbalkens
-        this.height = 10;  // Höhe des Lebensbalkens
+        this.width = 150;  // Breite des Lebensbalkens
+        this.height = 15;  // Höhe des Lebensbalkens
         this.offsetX = boss.width / 2 - this.width / 2;  // Zentriere den Balken über dem Boss
         this.offsetY = 50;  // Abstand über dem Kopf des Bosses
     }
 
     draw(ctx) {
         // Hintergrund des Balkens (verlorene HP)
-        ctx.fillStyle = 'red';
+        ctx.fillStyle = 'darkred';
         ctx.fillRect(this.boss.x + this.offsetX, this.boss.y + this.offsetY, this.width, this.height);
 
         // Vordere HP-Anzeige (verbleibende HP)
-        ctx.fillStyle = 'green';
+        ctx.fillStyle = 'limegreen';
         let currentWidth = this.width * (this.boss.health / this.boss.maxHealth);
         ctx.fillRect(this.boss.x + this.offsetX, this.boss.y + this.offsetY, currentWidth, this.height);
+
+        // Rahmen um die Healthbar zeichnen
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 2;
+        ctx.strokeRect(this.boss.x + this.offsetX, this.boss.y + this.offsetY, this.width, this.height);
     }
 }
